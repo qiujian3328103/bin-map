d09c4811e2c1abf53f783ff5a6ef7ed89d07d1eb
qiujian3328103 <abc3328103@163.com>
2023-06-07 23:26:23 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,9 @@ def card_component(title, content, color):
     st.markdown(
         f"""
         <div style='background-color: {color}; padding: 20px; border-radius: 10px; width: 150px;'>
-            <h3 style='text-align: center; color: white; font-size: 16px;'>{title}</h3>
-            <p style='color: white; margin-bottom: 0;'>Hold Lots: {content}</p>
-            <p style='color: white; margin-top: 0;'>Hold Wafers: {content}</p>
+            <h3 style='text-align: center; color: black; font-size: 20px;'>{title}</h3>
+            <p style='color: black; margin-bottom: 0;'>Hold Lots: {content}</p>
+            <p style='color: black; margin-top: 0;'>Hold Wafers: {content}</p>
         </div>
         """,
         unsafe_allow_html=True
@@ -18,7 +18,7 @@ def card_component(title, content, color):
 # Example usage
 title = "N2XM"
 content = "42"
-color = "#007bff"  # Change the color to your preferred value (e.g., #ff0000 for red)
+color = "#DAF7A6"  # Change the color to your preferred value (e.g., #ff0000 for red)
 
 st.write("# Streamlit Card Component Example")
 card_component(title, content, color)
