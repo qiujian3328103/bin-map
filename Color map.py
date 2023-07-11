65513f7b3ce8900b3b9b3ea382dd18dba6c17b38
qiujian3328103 <abc3328103@163.com>
2023-07-11 11:49:51 -0500

    Update Color map.py

--- Color map.py
+++ Color map.py
@@ -1,3 +1,53 @@
+def interpolate_color(start_color, end_color, fraction):
+    # Convert RGB colors to separate components
+    start_r, start_g, start_b = start_color
+    end_r, end_g, end_b = end_color
+    
+    # Interpolate the RGB components
+    interpolated_r = int(start_r + fraction * (end_r - start_r))
+    interpolated_g = int(start_g + fraction * (end_g - start_g))
+    interpolated_b = int(start_b + fraction * (end_b - start_b))
+    
+    # Return the interpolated color as a tuple
+    return (interpolated_r, interpolated_g, interpolated_b)
+
+def create_color_map(start_color, end_color, num_intervals):
+    color_map = []
+    
+    # Calculate the fraction step size
+    step = 1.0 / (num_intervals - 1)
+    
+    # Generate the color map by interpolating colors
+    for i in range(num_intervals):
+        fraction = i * step
+        color = interpolate_color(start_color, end_color, fraction)
+        color_map.append(color)
+    
+    return color_map
+
+def get_color_from_value(value, lst, color_map):
+    num_intervals = len(color_map)
+    
+    # Calculate the interval size
+    interval_size = max(lst) / (num_intervals - 1)
+    
+    # Calculate the interval index for the given value
+    interval_index = int(value / interval_size)
+    
+    # Handle values exceeding the maximum
+    if interval_index >= num_intervals - 1:
+        interval_index = num_intervals - 2
+    
+    # Interpolate the color based on the value
+    start_color = color_map[interval_index]
+    end_color = color_map[interval_index + 1]
+    fraction = (value - interval_index * interval_size) / interval_size
+    interpolated_color = interpolate_color(start_color, end_color, fraction)
+    
+    return interpolated_color
+    
+
+
 # Define the start and end colors
 start_color = (0, 255, 0)  # Green
 end_color = (255, 0, 0)    # Red
