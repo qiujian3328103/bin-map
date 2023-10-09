3d9218202d22d2965844dfae3144d06c8ae21673
qiujian3328103 <abc3328103@163.com>
2023-10-09 01:46:57 -0500

    Update test_shmoo_data.py

--- test_shmoo_data.py
+++ test_shmoo_data.py
@@ -107,3 +107,120 @@ test_shmoo_data = ["SHmmo TesT Program", "Test Site: 0", "AdjusTESTRange:Fasle",
 '30.00\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t#\t',
 'X-Aixs\t0.60\t0.61\t0.62\t0.63\t0.64\t0.65\t0.66\t0.67\t0.68\t0.69\t0.70\t0.71\t0.72\t0.73\t0.74\t0.75\t0.76\t0.77\t0.78\t0.79\t0.80\t0.81\t0.82\t0.83\t0.84\t0.85\t0.86\t0.87\t0.88\t0.89\t0.90\t0.91\t0.92\t0.93\t0.94\t0.95\t0.96\t0.97\t0.98\t0.99\t:VDDCX (v)',
 ]
+
+
+
+
+import pandas as pd 
+import numpy as np 
+import scipy.stats as stats
+import streamlit as st 
+import plotly.express as px
+import plotly.figure_factory as ff
+import scipy.stats as stats
+import plotly.subplots as sp
+import plotly.graph_objects as go
+
+df = pd.read_csv("data_sample.csv", index_col=False)
+df = df[["item_id", "item_value", "usl", "lsl"]]
+print(df.head(n=50))
+
+
+# Function to calculate metrics
+def calculate_metrics(filtered_df):
+    mean = filtered_df['item_value'].mean()
+    std = filtered_df['item_value'].std()
+    usl = filtered_df['usl'].iloc[0]
+    lsl = filtered_df['lsl'].iloc[0]
+    cpk = min((usl - mean) / (3 * std), (mean - lsl) / (3 * std))
+    total_num = len(filtered_df)
+    _, p_value = stats.normaltest(filtered_df['item_value'])
+    return mean, std, cpk, total_num, p_value, usl, lsl
+
+def plot_qqplot(data):
+    qq_data = stats.probplot(data, dist="norm")
+    x = np.array([qq_point[0] for qq_point in qq_data[0]])
+    y = np.array([qq_point[1] for qq_point in qq_data[0]])
+    fig = px.scatter(x=x, y=y)
+    fig.add_trace(px.line(x=qq_data[1][0] * x + qq_data[1][1], y=x).data[0])
+    fig.update_layout(title="Q-Q Plot",
+                      xaxis_title="Theoretical Quantiles",
+                      yaxis_title="Sample Quantiles")
+    return fig
+
+# Get unique item_id values and create a dropdown menu
+unique_items = df['item_id'].unique()
+selected_item = st.selectbox("Choose an item_id", unique_items)
+
+filtered_data = df[df["item_id"]==selected_item]
+# Calculating and displaying metrics
+
+mean, std, cpk, total_num, p_value, usl, lsl = calculate_metrics(filtered_data)
+st.markdown(f"""
+    **Mean:** {mean} <br>
+    **Std:** {std} <br>
+    **CpK:** {cpk} <br>
+    **Total Data Points:** {total_num} <br>
+    **P-Value:** {p_value} <br>
+    **USL:** {usl} <br>
+    **LSL:** {lsl}
+""", unsafe_allow_html=True)
+
+# # Subplot 1: Histogram with Normal Distribution Line
+# fig1 = px.histogram(filtered_data, x='item_value', nbins=20, marginal="box", 
+#                     title="Histogram with Normal Distribution")
+# fig1.add_trace(px.line(x=np.linspace(min(filtered_data['item_value']), max(filtered_data['item_value']), 100),
+#                        y=stats.norm.pdf(np.linspace(min(filtered_data['item_value']), max(filtered_data['item_value']), 100), mean, std),
+#                        ).data[0])
+# fig1.update_layout(showlegend=False)
+
+# # Subplot 2: CDF Plot
+# fig2 = px.histogram(filtered_data, x='item_value', cumulative=True, histnorm='probability',
+#                     title="Cumulative Distribution Function (CDF) Plot")
+# fig2.update_traces(marker_color='LightSkyBlue', marker_line_color='MediumPurple',
+#                    marker_line_width=1.5)
+# fig2.update_layout(showlegend=False)
+
+# # Subplot 3: Q-Q Plot
+# # normal_dist = np.random.normal(mean, std, 1000)
+# # fig3 = ff.create_qqplot(filtered_data['item_value'], dist=normal_dist, 
+# #                         title="Q-Q Plot")
+
+# fig3 = plot_qqplot(data=filtered_data["item_value"])
+
+
+# # Displaying plots in Streamlit
+# st.plotly_chart(fig1)
+# st.plotly_chart(fig2)
+# st.plotly_chart(fig3)
+
+# Create a subplot with 1 row and 3 columns
+fig = sp.make_subplots(rows=2, cols=2, subplot_titles=("Histogram", "CDF", "Q-Q Plot"))
+
+# Subplot 1: Histogram with Normal Distribution Line
+hist = px.histogram(filtered_data, x='item_value', nbins=20, marginal="box")
+hist_line = px.line(x=np.linspace(min(filtered_data['item_value']), max(filtered_data['item_value']), 100),
+                    y=stats.norm.pdf(np.linspace(min(filtered_data['item_value']), max(filtered_data['item_value']), 100), mean, std))
+fig.add_trace(go.Bar(x=hist.data[0].x, y=hist.data[0].y, marker_color='LightSkyBlue'), row=1, col=1)
+fig.add_trace(hist_line.data[0], row=1, col=1)
+fig.add_shape(dict(type="line", x0=lsl, x1=lsl, y0=0, y1=1, yref="paper", line=dict(color="Red")), row=1, col=1)
+fig.add_shape(dict(type="line", x0=usl, x1=usl, y0=0, y1=1, yref="paper", line=dict(color="Red")), row=1, col=1)
+
+# Subplot 2: CDF Plot
+cdf = px.histogram(filtered_data, x='item_value', cumulative=True, histnorm='probability')
+fig.add_trace(go.Bar(x=cdf.data[0].x, y=cdf.data[0].y, marker_color='LightSkyBlue'), row=1, col=2)
+fig.add_shape(dict(type="line", x0=lsl, x1=lsl, y0=0, y1=1, yref="paper", line=dict(color="Red")), row=1, col=2)
+fig.add_shape(dict(type="line", x0=usl, x1=usl, y0=0, y1=1, yref="paper", line=dict(color="Red")), row=1, col=2)
+
+# Subplot 3: Q-Q Plot
+qq_data = stats.probplot(filtered_data['item_value'], dist="norm")
+x = np.array([qq_point[0] for qq_point in qq_data[0]])
+y = np.array([qq_point[1] for qq_point in qq_data[0]])
+fig.add_trace(go.Scatter(x=x, y=y, mode='markers'), row=1, col=2)
+fig.add_trace(go.Line(x=x, y=qq_data[1][0] * x + qq_data[1][1]), row=1, col=2)
+
+# Update layout
+fig.update_layout(title_text="Various Plots", showlegend=False)
+
+# Displaying plots in Streamlit
+st.plotly_chart(fig)
